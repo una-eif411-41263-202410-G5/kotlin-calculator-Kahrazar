24245920d585a91dc230eeb7e9da8dafd7d27236
Kahrazar <diegoula08@hotmail.com>
2024-02-23 19:16:46 -0600

    Test[Class]: Probado metodos sumar, restar, multiplicar y dividir.

--- src/test/kotlin/design/patterns/calculadora/CalculadoraTest.kt
+++ src/test/kotlin/design/patterns/calculadora/CalculadoraTest.kt
@@ -14,10 +14,46 @@ class CalculadoraTest {
         val valorA = 5;
         val valorB = 2;
 
-        val resultadoFinal = calculadoraPrueba.sumar(5,2);
+        val resultadoFinal = calculadoraPrueba.sumar(valorA,valorB);
 
         assertNotNull(resultadoFinal);
         assertEquals(7, resultadoFinal);
     }
+
+    @Test
+    fun `Dado 2 valores numericos el metodo restar debe realizar la operacion correctamente`(){
+        val valorA = 5;
+        val valorB = 2;
+        val resultadoFinal = calculadoraPrueba.restar(valorA,valorB);
+
+        assertEquals(3, resultadoFinal);
+    }
+
+    @Test
+    fun `Dado 2 valores numericos el metodo multilplicar debe realizar la operacion correctamente`(){
+        val valorA = 5;
+        val valorB = 2;
+        val resultadoFinal = calculadoraPrueba.multiplicar(valorA,valorB);
+
+        assertEquals(3, resultadoFinal);
+    }
+
+    @Test
+    fun `Dada una division de 2 valores en el que su divisor sea cero el metodo dividir debe retornar cero`(){
+        val dividendo = 10F;
+        val divisor = 0F
+        val resultadoFinal = calculadoraPrueba.dividir(dividendo,divisor);
+
+        assertEquals(0F, resultadoFinal);
+    }
+    @Test
+    fun `Dada una division de 2 valores en el que sus operadores sean validos  el metodo dividir debe realizar la operacion`(){
+        val dividendo = 10F;
+        val divisor = 2F
+        val resultadoFinal = calculadoraPrueba.dividir(dividendo,divisor);
+
+        assertEquals(5F, resultadoFinal);
+    }
+
 
 }
