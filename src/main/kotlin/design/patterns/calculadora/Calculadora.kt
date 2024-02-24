fe3599f1afd94eb78f1dbf6fdfcc37b3e8e19d45
Kahrazar <diegoula08@hotmail.com>
2024-02-23 19:11:29 -0600

    Feat[Class]: Agregado metodos suma, resta, multiplicacion y division.

--- src/main/kotlin/design/patterns/calculadora/Calculadora.kt
+++ src/main/kotlin/design/patterns/calculadora/Calculadora.kt
@@ -1,4 +1,21 @@
 package design.patterns.design.patterns.calculadora
 
 class Calculadora {
+    fun sumar(a: Int, b: Int): Int{
+        return a + b;
+    }
+
+    //Si la funcion es de una sola linea se puede simplificar asi
+    fun restar(a:Int, b:Int) = a - b;
+
+    fun multiplicar(a:Int, b:Int) = a*b;
+
+    fun dividir(dividendo: Float, divisor: Float):Float{
+        return if(divisor <= 0){
+            0F;
+        }else{
+            dividendo/divisor;
+        }
+    }
+
 }
--- src/test/kotlin/design/patterns/calculadora/CalculadoraTest.kt
+++ src/test/kotlin/design/patterns/calculadora/CalculadoraTest.kt
@@ -1,4 +1,23 @@
 package design.patterns.calculadora
 
+import design.patterns.design.patterns.calculadora.Calculadora
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
 class CalculadoraTest {
+
+    private val calculadoraPrueba = Calculadora();
+
+    @Test
+    fun `Dado 2 valores numericos debe sumar correctamete`(){
+        val valorA = 5;
+        val valorB = 2;
+
+        val resultadoFinal = calculadoraPrueba.sumar(5,2);
+
+        assertNotNull(resultadoFinal);
+        assertEquals(7, resultadoFinal);
+    }
+
 }
